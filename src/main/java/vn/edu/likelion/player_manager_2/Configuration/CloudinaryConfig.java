a9681061c756118c6ad40cc1cdc51a775922792e
= <=>
2024-08-14 11:36:55 +0700

    hardcode cloud api 123

--- src/main/java/vn/edu/likelion/player_manager_2/Configuration/CloudinaryConfig.java
+++ src/main/java/vn/edu/likelion/player_manager_2/Configuration/CloudinaryConfig.java
@@ -2,7 +2,6 @@ package vn.edu.likelion.player_manager_2.Configuration;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
